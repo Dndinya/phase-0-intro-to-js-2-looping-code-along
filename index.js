3182f14db88e873fa44cd359a7bf0b49ee487aa8
Derrick <derrick.ndinya@student.moringaschool.com>
2023-12-09 19:34:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(cards){
+    const arrY = [];
+    for (let i = 0; i < cards.length; i++) {
+         arrY[i] = (`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+    return arrY;
+}
+function countDown(startingNumber){
+    while (startingNumber >= 0) {
+        console.log(startingNumber);
+        startingNumber--;
+    }
+}
